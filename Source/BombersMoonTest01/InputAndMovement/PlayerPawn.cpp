d642a65c3181467a41f437141f588ce88ccb9c13
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-03-07 22:11:22 -0700

    Begin on implementing physics on the player

--- Source/BombersMoonTest01/InputAndMovement/PlayerPawn.cpp
+++ Source/BombersMoonTest01/InputAndMovement/PlayerPawn.cpp
@@ -23,7 +23,11 @@ void APlayerPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UpdatePosition();
 }
+
+/* ----------- *\
+\* \/ Input \/ */
 
 // Called to bind functionality to input
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -32,3 +36,64 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 }
 
+/**
+ * Input action of moving forward
+ *
+ * @param Scale - A number from -1 to 1 that defines how much moving forward is being inputted
+ */
+void APlayerPawn::InputMoveForward(float Scale)
+{
+	FVector VelocityToAdd = FVector(1, 0, 0).RotateAngleAxis(GetActorRotation().Yaw, (FVector(0, 0, 1))) * Scale * WalkAcceleration;
+	if (VelocityToAdd.Length > MaxWalkSpeed)
+	{
+		VelocityToAdd.Normalize();
+	}
+	AddVelocity(FVector(1, 0, 0).RotateAngleAxis(GetActorRotation().Yaw, (FVector(0, 0, 1))) * Scale * WalkAcceleration);
+}
+
+/**
+ * Input action of moving right
+ *
+ * @param Scale - A number from -1 to 1 that defines how much moving forward is being inputted
+ */
+void APlayerPawn::InputMoveRight(float Scale)
+{
+	AddVelocity(FVector(0, 1, 0).RotateAngleAxis(GetActorRotation().Yaw, (FVector(0, 0, 1))) * Scale * WalkAcceleration);
+}
+
+/* /\ Input /\ *\
+\* ----------- */
+
+/* ------------- *\
+\* \/ Physics \/ */
+
+/**
+ * Updates the position of the player based on velocity
+ */
+void APlayerPawn::UpdatePosition()
+{
+	SetActorLocation(Velocity + GetActorLocation());
+}
+
+/**
+ * Adds velocity to the velocity of the player
+ *
+ * @param AddedVelocity - The added velocity
+ */
+void APlayerPawn::AddVelocity(FVector AddedVelocity)
+{
+	Velocity += AddedVelocity;
+}
+
+/**
+ * Sets the velocity of the player
+ *
+ * @param NewVelocity - The new velocity
+ */
+void APlayerPawn::SetVelocity(FVector NewVelocity)
+{
+	Velocity = NewVelocity;
+}
+
+/* /\ Physics /\ *\
+\* ------------- */
