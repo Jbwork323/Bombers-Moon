b03f2882f0ea51bbcdcc6a5b173e438c887c0bf6
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-03-10 20:40:39 -0700

    why

--- Source/BombersMoonTest01/PlayerCharacter.h
+++ Source/BombersMoonTest01/PlayerCharacter.h
@@ -11,6 +11,9 @@ class BOMBERSMOONTEST01_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	/* -------------------- *\
+	\* \/ Initialization \/ */
+
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
@@ -20,10 +23,83 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/* /\ Initialization /\ *\
+	\* -------------------- */
+
+	/* ---------- *\
+	\* \/ Tick \/ */
+
+public:
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/* /\ Tick /\ *\
+	\* ---------- */
+
+	/* ------------- *\
+	\* \/ Walking \/ */
+
+private:
+
+	/**
+	 * Input axis for recieving the move forward input
+	 * 
+	 * @param AxisValue - The value of the input axis
+	 */
+	void InputAxisMoveForward(float AxisValue);
+
+	/**
+	 * Input axis for recieving the move right input
+	 *
+	 * @param AxisValue - The value of the input axis
+	 */
+	void InputAxisMoveRight(float AxisValue);
+
+	/* /\ Walking /\ *\
+	\* ------------- */
+
+	/* ------------- *\
+	\* \/ Looking \/ */
+
+private:
+
+	/**
+	 * Input axis for recieving the turn input
+	 * 
+	 * @param AxisValue - The value of the input axis
+	 */
+	void InputAxisTurn(float AxisValue);
+
+	/**
+	 * Input axis for recieving the look up input
+	 *
+	 * @param AxisValue - The value of the input axis
+	 */
+	void InputAxisLookUp(float AxisValue);
+
+	/* /\ Looking /\ *\
+	\* ------------- */
+
+	/* ------------- *\
+	\* \/ Jumping \/ */
+
+private:
+
+	/**
+	 * Input action for recieving the jump input
+	 */
+	void InputActionJump();
+
+	/**
+	 * Launches the character into the air
+	 */
+	void Jump();
+
+	/* /\ Jumping /\ *\
+	\* ------------- */
 };
